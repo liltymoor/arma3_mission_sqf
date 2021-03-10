3653fd28ce57bae7f22e65c5926fe861add68a4f
liltymoor <timur.chelter@mail.ru>
2021-03-10 23:51:19 +0300

    Update ProfileDialog.hpp

--- dialogs/ProfileDialog.hpp
+++ dialogs/ProfileDialog.hpp
@@ -2,7 +2,7 @@
 class ProfileDialog
 {
 	idd = 15000;
-	onLoad = "escKeyEH = (_this select 0) displayAddEventHandler [""KeyDown"", ""if (((_this select 1) == 21)) then {true};""]; _this call PENA_RAID_LOBBY_CHECKER; _this call FRAME_03_Load;"
+	onLoad = "escKeyEH = (_this select 0) displayAddEventHandler [""KeyDown"", ""if (((_this select 1) == 21)) then {true};""]; _this call FRAME_03_Load;"
 	class ControlsBackground
 	{
 		class Background
@@ -295,7 +295,7 @@ class ProfileDialog
 		class Invite
 		{
 			type = 1;
-			idc = -1;
+			idc = 15001;
 			x = safeZoneX + safeZoneW * 0.59375;
 			y = safeZoneY + safeZoneH * 0.39555556;
 			w = safeZoneW * 0.0875;
@@ -321,6 +321,7 @@ class ProfileDialog
 			soundEnter[] = {"\A3\ui_f\data\sound\RscButton\soundEnter",0.09,1.0};
 			soundEscape[] = {"\A3\ui_f\data\sound\RscButton\soundEscape",0.09,1.0};
 			soundPush[] = {"\A3\ui_f\data\sound\RscButton\soundPush",0.09,1.0};
+			action = "[15001]remoteExec['PENA_SEND_INVITE', player, false];";
 			
 		};
 		class PlayersInSquad
@@ -350,39 +351,40 @@ class ProfileDialog
 				border = "\A3\ui_f\data\gui\cfg\scrollbar\border_ca.paa";
 				
 			};
+
 			
 		};
-		class quit
-		{
-			type = 1;
-			idc = -1;
-			x = safeZoneX + safeZoneW * 0.59375;
-			y = safeZoneY + safeZoneH * 0.61555556;
-			w = safeZoneW * 0.0875;
-			h = safeZoneH * 0.03666667;
-			style = 0+2;
-			text = "Выйти из группы";
-			borderSize = 0;
-			colorBackground[] = {0.502,0.6,1,1};
-			colorBackgroundActive[] = {0.502,0.6,1,1};
-			colorBackgroundDisabled[] = {0.502,0.6,1,1};
-			colorBorder[] = {0,0,0,0};
-			colorDisabled[] = {0,0,0,0};
-			colorFocused[] = {0.502,0.6,1,1};
-			colorShadow[] = {0,0,0,0};
-			colorText[] = {1,1,1,1};
-			font = "PuristaMedium";
-			offsetPressedX = 0.01;
-			offsetPressedY = 0.01;
-			offsetX = 0.01;
-			offsetY = 0.01;
-			sizeEx = (((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 1);
-			soundClick[] = {"\A3\ui_f\data\sound\RscButton\soundClick",0.09,1.0};
-			soundEnter[] = {"\A3\ui_f\data\sound\RscButton\soundEnter",0.09,1.0};
-			soundEscape[] = {"\A3\ui_f\data\sound\RscButton\soundEscape",0.09,1.0};
-			soundPush[] = {"\A3\ui_f\data\sound\RscButton\soundPush",0.09,1.0};
-			
-		};
+		class create
+        {
+            type = 1;
+            idc = 15002;
+            x = safeZoneX + safeZoneW * 0.59375;
+            y = safeZoneY + safeZoneH * 0.61555556;
+            w = safeZoneW * 0.0875;
+            h = safeZoneH * 0.03666667;
+            style = 0+2;
+            text = "Создать группу";
+            borderSize = 0;
+            colorBackground[] = {0.502,0.6,1,1};
+            colorBackgroundActive[] = {1,1,1,1};
+            colorBackgroundDisabled[] = {1,1,1,1};
+            colorBorder[] = {0,0,0,0};
+            colorDisabled[] = {0.2,0.2,0.2,1};
+            colorFocused[] = {0.2,0.2,0.2,1};
+            colorShadow[] = {0,0,0,1};
+            colorText[] = {1,1,1,1};
+            font = "PuristaMedium";
+            offsetPressedX = 0.01;
+            offsetPressedY = 0.01;
+            offsetX = 0.01;
+            offsetY = 0.01;
+            sizeEx = (((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 1);
+            soundClick[] = {"\A3\ui_f\data\sound\RscButton\soundClick",0.09,1.0};
+            soundEnter[] = {"\A3\ui_f\data\sound\RscButton\soundEnter",0.09,1.0};
+            soundEscape[] = {"\A3\ui_f\data\sound\RscButton\soundEscape",0.09,1.0};
+            soundPush[] = {"\A3\ui_f\data\sound\RscButton\soundPush",0.09,1.0};
+            action = "[15002]remoteExec['PENA_CREATE_PARTY', player, false];";
+        };
 		
 	};
 	
