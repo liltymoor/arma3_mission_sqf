57886aaaac1e60979b1b6f6106889c488348c12c
DaddyOfCoding <st_ks01@list.ru>
2021-03-10 20:43:22 +0300

    Update ProfileDialog.hpp

--- bratva.Altis/dialogs/ProfileDialog.hpp
+++ bratva.Altis/dialogs/ProfileDialog.hpp
@@ -263,6 +263,126 @@ class ProfileDialog
 			action = "call Freddy_fnc_pickRole;";
 			
 		};
+			class OnlinePlayers
+		{
+			type = 5;
+			idc = -1;
+			x = safeZoneX + safeZoneW * 0.59375;
+			y = safeZoneY + safeZoneH * 0.24888889;
+			w = safeZoneW * 0.0875;
+			h = safeZoneH * 0.12888889;
+			style = 16;
+			colorBackground[] = {0,0,0,0.7};
+			colorDisabled[] = {0,0,0,0.7};
+			colorSelect[] = {1,1,0.302,1};
+			colorText[] = {1,1,1,1};
+			font = "PuristaMedium";
+			maxHistoryDelay = 0;
+			rowHeight = 0;
+			sizeEx = (((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 1);
+			soundSelect[] = {"\A3\ui_f\data\sound\RscListbox\soundSelect",0.09,1.0};
+			class ListScrollBar
+			{
+				color[] = {1,1,1,1};
+				thumb = "\A3\ui_f\data\gui\cfg\scrollbar\thumb_ca.paa";
+				arrowFull = "\A3\ui_f\data\gui\cfg\scrollbar\arrowFull_ca.paa";
+				arrowEmpty = "\A3\ui_f\data\gui\cfg\scrollbar\arrowEmpty_ca.paa";
+				border = "\A3\ui_f\data\gui\cfg\scrollbar\border_ca.paa";
+				
+			};
+			
+		};
+		class Invite
+		{
+			type = 1;
+			idc = -1;
+			x = safeZoneX + safeZoneW * 0.59375;
+			y = safeZoneY + safeZoneH * 0.39555556;
+			w = safeZoneW * 0.0875;
+			h = safeZoneH * 0.03666667;
+			style = 0+2;
+			text = "Пригласить";
+			borderSize = 0;
+			colorBackground[] = {0.502,0.6,1,1};
+			colorBackgroundActive[] = {0.502,0.6,1,1};
+			colorBackgroundDisabled[] = {0.502,0.6,1,1};
+			colorBorder[] = {0,0,0,0};
+			colorDisabled[] = {0,0,0,0};
+			colorFocused[] = {0.502,0.6,1,1};
+			colorShadow[] = {0,0,0,0};
+			colorText[] = {1,1,1,1};
+			font = "PuristaMedium";
+			offsetPressedX = 0.01;
+			offsetPressedY = 0.01;
+			offsetX = 0.01;
+			offsetY = 0.01;
+			sizeEx = (((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 1);
+			soundClick[] = {"\A3\ui_f\data\sound\RscButton\soundClick",0.09,1.0};
+			soundEnter[] = {"\A3\ui_f\data\sound\RscButton\soundEnter",0.09,1.0};
+			soundEscape[] = {"\A3\ui_f\data\sound\RscButton\soundEscape",0.09,1.0};
+			soundPush[] = {"\A3\ui_f\data\sound\RscButton\soundPush",0.09,1.0};
+			
+		};
+		class PlayersInSquad
+		{
+			type = 5;
+			idc = -1;
+			x = safeZoneX + safeZoneW * 0.59375;
+			y = safeZoneY + safeZoneH * 0.46888889;
+			w = safeZoneW * 0.0875;
+			h = safeZoneH * 0.12888889;
+			style = 16;
+			colorBackground[] = {0,0,0,0.7};
+			colorDisabled[] = {0,0,0,0.7};
+			colorSelect[] = {1,1,0.302,1};
+			colorText[] = {1,1,1,1};
+			font = "PuristaMedium";
+			maxHistoryDelay = 0;
+			rowHeight = 0;
+			sizeEx = (((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 1);
+			soundSelect[] = {"\A3\ui_f\data\sound\RscListbox\soundSelect",0.09,1.0};
+			class ListScrollBar
+			{
+				color[] = {1,1,1,1};
+				thumb = "\A3\ui_f\data\gui\cfg\scrollbar\thumb_ca.paa";
+				arrowFull = "\A3\ui_f\data\gui\cfg\scrollbar\arrowFull_ca.paa";
+				arrowEmpty = "\A3\ui_f\data\gui\cfg\scrollbar\arrowEmpty_ca.paa";
+				border = "\A3\ui_f\data\gui\cfg\scrollbar\border_ca.paa";
+				
+			};
+			
+		};
+		class quit
+		{
+			type = 1;
+			idc = -1;
+			x = safeZoneX + safeZoneW * 0.59375;
+			y = safeZoneY + safeZoneH * 0.61555556;
+			w = safeZoneW * 0.0875;
+			h = safeZoneH * 0.03666667;
+			style = 0+2;
+			text = "Выйти из группы";
+			borderSize = 0;
+			colorBackground[] = {0.502,0.6,1,1};
+			colorBackgroundActive[] = {0.502,0.6,1,1};
+			colorBackgroundDisabled[] = {0.502,0.6,1,1};
+			colorBorder[] = {0,0,0,0};
+			colorDisabled[] = {0,0,0,0};
+			colorFocused[] = {0.502,0.6,1,1};
+			colorShadow[] = {0,0,0,0};
+			colorText[] = {1,1,1,1};
+			font = "PuristaMedium";
+			offsetPressedX = 0.01;
+			offsetPressedY = 0.01;
+			offsetX = 0.01;
+			offsetY = 0.01;
+			sizeEx = (((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 1);
+			soundClick[] = {"\A3\ui_f\data\sound\RscButton\soundClick",0.09,1.0};
+			soundEnter[] = {"\A3\ui_f\data\sound\RscButton\soundEnter",0.09,1.0};
+			soundEscape[] = {"\A3\ui_f\data\sound\RscButton\soundEscape",0.09,1.0};
+			soundPush[] = {"\A3\ui_f\data\sound\RscButton\soundPush",0.09,1.0};
+			
+		};
 		
 	};
 	
