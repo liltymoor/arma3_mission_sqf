a01180818ba280e1206c24870f65126ce696f654
liltymoor <timur.chelter@mail.ru>
2021-03-14 16:46:14 +0300

    Доделали группы
    мб багиии будут остерегайтесь акул + китов

--- dialogs/SquadDialog.hpp
+++ dialogs/SquadDialog.hpp
@@ -1,8 +1,8 @@
 #include "CustomControlClasses.h"
 class SquadDialog
 {
-	idd = -1;
-	
+	idd = 16000;
+	onLoad = "escKeyEH = (_this select 0) displayAddEventHandler [""KeyDown"", ""if (((_this select 1) == 21)) then {true};""]; _this call PENA_PARTY_ONLOAD;"
 	class ControlsBackground
 	{
 		class Background
@@ -14,39 +14,39 @@ class SquadDialog
 			w = safeZoneW * 0.6125;
 			h = safeZoneH * 1.03666667;
 			style = 48;
-			text = "C:\Dialog creator\staff\tabletempty.png";
+			text = "images\tabletempty.paa";
 			colorBackground[] = {1,1,1,1};
 			colorText[] = {1,1,1,1};
 			font = "PuristaMedium";
 			sizeEx = (((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 1);
 			
 		};
-		class SuqadMembersText
+		class OnlinePlayersText
 		{
 			type = 0;
 			idc = -1;
-			x = safeZoneX + safeZoneW * 0.519375;
-			y = safeZoneY + safeZoneH * 0.25777778;
+			x = safeZoneX + safeZoneW * 0.29375;
+			y = safeZoneY + safeZoneH * 0.24888889;
 			w = safeZoneW * 0.1875;
 			h = safeZoneH * 0.03666667;
 			style = 2;
-			text = "Список участников отряда";
+			text = "Игроки онлайн";
 			colorBackground[] = {0,0,0,0.7};
 			colorText[] = {1,1,1,1};
 			font = "PuristaMedium";
 			sizeEx = (((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 1);
 			
 		};
-		class PlayersWithoutSquadText
+		class SquadMembersText
 		{
 			type = 0;
 			idc = -1;
-			x = safeZoneX + safeZoneW * 0.294375;
-			y = safeZoneY + safeZoneH * 0.25777778;
+			x = safeZoneX + safeZoneW * 0.51875;
+			y = safeZoneY + safeZoneH * 0.24888889;
 			w = safeZoneW * 0.1875;
 			h = safeZoneH * 0.03666667;
 			style = 2;
-			text = "Список игроков без группы";
+			text = "Игроки в отряде";
 			colorBackground[] = {0,0,0,0.7};
 			colorText[] = {1,1,1,1};
 			font = "PuristaMedium";
@@ -57,49 +57,20 @@ class SquadDialog
 	};
 	class Controls
 	{
-		class SquadMembersListBox
+		class SquadMembers
 		{
 			type = 5;
-			idc = -1;
+			idc = 16003;
 			x = safeZoneX + safeZoneW * 0.51875;
-			y = safeZoneY + safeZoneH * 0.32222223;
+			y = safeZoneY + safeZoneH * 0.29777778;
 			w = safeZoneW * 0.1875;
-			h = safeZoneH * 0.27555556;
+			h = safeZoneH * 0.32444445;
 			style = 16;
-			colorBackground[] = {0,0,0,0.7059};
-			colorDisabled[] = {1,1,1,1};
-			colorSelect[] = {1,1,1,1};
-			colorText[] = {1,1,0.302,1};
-			font = "TahomaB";
-			maxHistoryDelay = 0;
-			rowHeight = 0;
-			sizeEx = (((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 1);
-			soundSelect[] = {"\A3\ui_f\data\sound\RscListbox\soundSelect",0.09,1.0};
-			class ListScrollBar
-			{
-				color[] = {1,1,1,1};
-				thumb = "\A3\ui_f\data\gui\cfg\scrollbar\thumb_ca.paa";
-				arrowFull = "\A3\ui_f\data\gui\cfg\scrollbar\arrowFull_ca.paa";
-				arrowEmpty = "\A3\ui_f\data\gui\cfg\scrollbar\arrowEmpty_ca.paa";
-				border = "\A3\ui_f\data\gui\cfg\scrollbar\border_ca.paa";
-				
-			};
-			
-		};
-		class PlayersWithoutSquadListBox
-		{
-			type = 5;
-			idc = -1;
-			x = safeZoneX + safeZoneW * 0.29375;
-			y = safeZoneY + safeZoneH * 0.32222223;
-			w = safeZoneW * 0.1875;
-			h = safeZoneH * 0.27555556;
-			style = 16;
-			colorBackground[] = {0,0,0,0.7059};
-			colorDisabled[] = {1,1,1,1};
-			colorSelect[] = {1,1,1,1};
-			colorText[] = {1,1,0.302,1};
-			font = "TahomaB";
+			colorBackground[] = {0,0,0,0.7};
+			colorDisabled[] = {0,0,0,0.7};
+			colorSelect[] = {1,1,0.302,1};
+			colorText[] = {1,1,1,1};
+			font = "PuristaMedium";
 			maxHistoryDelay = 0;
 			rowHeight = 0;
 			sizeEx = (((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 1);
@@ -118,23 +89,23 @@ class SquadDialog
 		class Invite
 		{
 			type = 1;
-			idc = -1;
-			x = safeZoneX + safeZoneW * 0.2925;
-			y = safeZoneY + safeZoneH * 0.63222223;
+			idc = 16001;
+			x = safeZoneX + safeZoneW * 0.29375;
+			y = safeZoneY + safeZoneH * 0.64;
 			w = safeZoneW * 0.0875;
 			h = safeZoneH * 0.03666667;
 			style = 0+2;
 			text = "Пригласить";
 			borderSize = 0;
-			colorBackground[] = {0.4,0.502,0.902,0.7059};
-			colorBackgroundActive[] = {0.4,0.502,0.902,0.7059};
-			colorBackgroundDisabled[] = {0.2,0.2,0.2,0};
+			colorBackground[] = {0.502,0.6,1,1};
+			colorBackgroundActive[] = {1,1,1,1};
+			colorBackgroundDisabled[] = {1,1,1,1};
 			colorBorder[] = {0,0,0,0};
-			colorDisabled[] = {0.2,0.2,0.2,0};
-			colorFocused[] = {0.2,0.2,0.2,0};
-			colorShadow[] = {0,0,0,0};
+			colorDisabled[] = {0.2,0.2,0.2,1};
+			colorFocused[] = {0.2,0.2,0.2,1};
+			colorShadow[] = {0,0,0,1};
 			colorText[] = {1,1,1,1};
-			font = "TahomaB";
+			font = "PuristaMedium";
 			offsetPressedX = 0.01;
 			offsetPressedY = 0.01;
 			offsetX = 0.01;
@@ -144,28 +115,88 @@ class SquadDialog
 			soundEnter[] = {"\A3\ui_f\data\sound\RscButton\soundEnter",0.09,1.0};
 			soundEscape[] = {"\A3\ui_f\data\sound\RscButton\soundEscape",0.09,1.0};
 			soundPush[] = {"\A3\ui_f\data\sound\RscButton\soundPush",0.09,1.0};
+			action = "[16001]remoteExec['PENA_SEND_INVITE', player, false];";
+		};
+		class OnlinePlayers
+		{
+			type = 5;
+			idc = 16666;
+			x = safeZoneX + safeZoneW * 0.29375;
+			y = safeZoneY + safeZoneH * 0.29777778;
+			w = safeZoneW * 0.1875;
+			h = safeZoneH * 0.32444445;
+			style = 16;
+			colorBackground[] = {0,0,0,0.7};
+			colorDisabled[] = {0,0,0,0.7};
+			colorSelect[] = {1,1,0.302,1};
+			colorText[] = {1,1,1,1};
+			font = "PuristaMedium";
+			maxHistoryDelay = 0;
+			rowHeight = 0;
+			sizeEx = (((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 1);
+			soundSelect[] = {"\A3\ui_f\data\sound\RscListbox\soundSelect",0.09,1.0};
+			class ListScrollBar
+			{
+				color[] = {1,1,1,1};
+				thumb = "\A3\ui_f\data\gui\cfg\scrollbar\thumb_ca.paa";
+				arrowFull = "\A3\ui_f\data\gui\cfg\scrollbar\arrowFull_ca.paa";
+				arrowEmpty = "\A3\ui_f\data\gui\cfg\scrollbar\arrowEmpty_ca.paa";
+				border = "\A3\ui_f\data\gui\cfg\scrollbar\border_ca.paa";
+				
+			};
 			
+		};
+		class Create
+		{
+			type = 1;
+			idc = 16002;
+			x = safeZoneX + safeZoneW * 0.51875;
+			y = safeZoneY + safeZoneH * 0.64;
+			w = safeZoneW * 0.0875;
+			h = safeZoneH * 0.03666667;
+			style = 0+2;
+			text = "Создать";
+			borderSize = 0;
+			colorBackground[] = {0.502,0.6,1,1};
+			colorBackgroundActive[] = {1,1,1,1};
+			colorBackgroundDisabled[] = {1,1,1,1};
+			colorBorder[] = {0,0,0,0};
+			colorDisabled[] = {0.2,0.2,0.2,1};
+			colorFocused[] = {0.2,0.2,0.2,1};
+			colorShadow[] = {0,0,0,1};
+			colorText[] = {1,1,1,1};
+			font = "PuristaMedium";
+			offsetPressedX = 0.01;
+			offsetPressedY = 0.01;
+			offsetX = 0.01;
+			offsetY = 0.01;
+			sizeEx = (((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 1);
+			soundClick[] = {"\A3\ui_f\data\sound\RscButton\soundClick",0.09,1.0};
+			soundEnter[] = {"\A3\ui_f\data\sound\RscButton\soundEnter",0.09,1.0};
+			soundEscape[] = {"\A3\ui_f\data\sound\RscButton\soundEscape",0.09,1.0};
+			soundPush[] = {"\A3\ui_f\data\sound\RscButton\soundPush",0.09,1.0};
+			action = "[16002]remoteExec['PENA_CREATE_PARTY', player, false];";
 		};
 		class Quit
 		{
 			type = 1;
-			idc = -1;
-			x = safeZoneX + safeZoneW * 0.6175;
-			y = safeZoneY + safeZoneH * 0.63222223;
+			idc = 16004;
+			x = safeZoneX + safeZoneW * 0.61875;
+			y = safeZoneY + safeZoneH * 0.64;
 			w = safeZoneW * 0.0875;
 			h = safeZoneH * 0.03666667;
 			style = 0+2;
 			text = "Выйти из группы";
 			borderSize = 0;
-			colorBackground[] = {0.4,0.502,0.902,0.7059};
-			colorBackgroundActive[] = {0.4,0.502,0.902,0.7059};
-			colorBackgroundDisabled[] = {0.2,0.2,0.2,0};
+			colorBackground[] = {0.502,0.6,1,1};
+			colorBackgroundActive[] = {1,1,1,1};
+			colorBackgroundDisabled[] = {1,1,1,1};
 			colorBorder[] = {0,0,0,0};
-			colorDisabled[] = {0.2,0.2,0.2,0};
-			colorFocused[] = {0.2,0.2,0.2,0};
-			colorShadow[] = {0,0,0,0};
+			colorDisabled[] = {0.2,0.2,0.2,1};
+			colorFocused[] = {0.2,0.2,0.2,1};
+			colorShadow[] = {0,0,0,1};
 			colorText[] = {1,1,1,1};
-			font = "TahomaB";
+			font = "PuristaMedium";
 			offsetPressedX = 0.01;
 			offsetPressedY = 0.01;
 			offsetX = 0.01;
@@ -175,7 +206,7 @@ class SquadDialog
 			soundEnter[] = {"\A3\ui_f\data\sound\RscButton\soundEnter",0.09,1.0};
 			soundEscape[] = {"\A3\ui_f\data\sound\RscButton\soundEscape",0.09,1.0};
 			soundPush[] = {"\A3\ui_f\data\sound\RscButton\soundPush",0.09,1.0};
-			
+			action = "[16004]remoteExec['PENA_PARTY_LEAVE', player, false];";
 		};
 		
 	};
